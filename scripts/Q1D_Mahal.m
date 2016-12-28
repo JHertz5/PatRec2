614abd27472459c98dec2fa6391904303f665979
JHertz5 <jukka.hertzog@live.co.uk>
2016-12-28 01:35:09 +0000

    Completed Mahalanobis Distance

--- scripts/Q1D_Mahal.m
+++ scripts/Q1D_Mahal.m
@@ -1,7 +1,7 @@
 %%
 
 clc
-clear all
+clear variables
 close all
 
 %add data directory to path
@@ -17,31 +17,81 @@ end
 load wine_separatedData.mat
 load wine_covMatrix
 
-%% Mahalanobis Distance Using Covraince Matrices from B.b
+%% Mahalanobis Distance Using Individual Class Covariance Matrices from B.b
 
-class1 = find(training_classes == 1);
-class2 = find(training_classes == 2);
-class3 = find(training_classes == 3);
+numTraining = length(training_classes);
+numTesting = length(testing_classes);
 
-Mahalclasses = zeros(1,length(testing_norm));
+mahalDistance_cov1 = zeros(numTesting, numTraining);
+mahalDistance_cov2 = zeros(numTesting, numTraining);
+mahalDistance_cov3 = zeros(numTesting, numTraining);
+mahalDistance_covAll = zeros(numTesting, numTraining);
 
-% this is the same as doing (x-m)^T cov^-1 (x-m) for each cov matrix
-w(1,:) = mahal(testing_norm(:,:),training_norm(class1,:));
-w(2,:) = mahal(testing_norm(:,:),training_norm(class2,:));
-w(3,:) = mahal(testing_norm(:,:),training_norm(class3,:));
+mahalClass_cov1 = zeros(1,numTesting);
+mahalClass_cov2 = zeros(1,numTesting);
+mahalClass_cov3 = zeros(1,numTesting);
+mahalClass_covAll = zeros(1,numTesting);
 
-
-for i = 1:length(testing_norm)
-    switch min(w(:,i))
-        case w(1,i)
-            Mahalclasses(i) = 1;
-        case w(2,i)
-            Mahalclasses(i) = 2;
-        case w(3,i)
-            Mahalclasses(i) = 3;
-    end
+for testingIndex = 1:numTesting
+    %test one vector of testing data at a time
+    
+    % Class 1 covariance matrix
+    G = chol(cov_1Norm^-1);
+    mahalDistance_cov1(testingIndex,:) = sum((G*testing_norm(testingIndex,:)' - G*training_norm').^2);
+    [~,minIndex] = min(mahalDistance_cov1(testingIndex,:));
+    mahalClass_cov1(testingIndex) = training_classes(minIndex);
+    
+    % Class 2 covariance matrix
+    G = chol(cov_2Norm^-1);
+    mahalDistance_cov2(testingIndex,:) = sum((G*testing_norm(testingIndex,:)' - G*training_norm').^2);
+    [~,minIndex] = min(mahalDistance_cov2(testingIndex,:));
+    mahalClass_cov2(testingIndex) = training_classes(minIndex);
+    
+    % Class 3 covariance matrix
+    G = chol(cov_3Norm^-1);
+    mahalDistance_cov3(testingIndex,:) = sum((G*testing_norm(testingIndex,:)' - G*training_norm').^2);
+    [~,minIndex] = min(mahalDistance_cov3(testingIndex,:));
+    mahalClass_cov3(testingIndex) = training_classes(minIndex);
+    
+    % Full covariance matrix
+    G = chol(cov_allNorm^-1);
+    mahalDistance_covAll(testingIndex,:) = sum((G*testing_norm(testingIndex,:)' - G*training_norm').^2);
+    [~,minIndex] = min(mahalDistance_covAll(testingIndex,:));
+    mahalClass_covAll(testingIndex) = training_classes(minIndex);
 end
 
-MahalAccNorm = (length(testing_norm)-nnz(Mahalclasses - testing_classes))*100/length(testing_norm);
+%% Evaluate results
 
-%% %% Mahalanobis Distance Using Covraince Matrices from B.b
+% Find failure indices
+mahalFailureIndices_cov1 = find(testing_classes ~= mahalClass_cov1);
+mahalFailureIndices_cov2 = find(testing_classes ~= mahalClass_cov2);
+mahalFailureIndices_cov3 = find(testing_classes ~= mahalClass_cov3);
+mahalFailureIndices_covAll = find(testing_classes ~= mahalClass_covAll);
+
+mahalSuccess_cov1 = ones(1,numTesting);
+mahalSuccess_cov1(mahalFailureIndices_cov1) = 0;
+
+mahalSuccess_cov2 = ones(1,numTesting);
+mahalSuccess_cov2(mahalFailureIndices_cov2) = 0;
+
+mahalSuccess_cov3 = ones(1,numTesting);
+mahalSuccess_cov3(mahalFailureIndices_cov3) = 0;
+
+mahalSuccess_covAll = ones(1,numTesting);
+mahalSuccess_covAll(mahalFailureIndices_covAll) = 0;
+
+mahalAcc_cov1 = sum(mahalSuccess_cov1)*100/numTesting;
+mahalAcc_cov2 = sum(mahalSuccess_cov2)*100/numTesting;
+mahalAcc_cov3 = sum(mahalSuccess_cov3)*100/numTesting;
+mahalAcc_covAll = sum(mahalSuccess_covAll)*100/numTesting;
+
+%% Plot graphs
+
+figure
+plot(mahalDistance_cov1(1,:))
+ylabel('Mahalanobis Distance','interpreter','latex','fontsize',30)
+xlabel('Training Data Index','interpreter','latex','fontsize',30)
+line([40 40], ylim, 'Color', 'black')
+line([87 87], ylim, 'Color', 'black')
+
+
